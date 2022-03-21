46497bc9be5bb7f6f05bacc226b8087281097d88
rafaelarissa <rafaela.larissalj@gmail.com>
2022-03-21 02:58:13 -0300

    feat: added search order by client id

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -4,14 +4,47 @@ export async function setClients(req, res) {
   const { name, address, phone } = req.body;
 
   try {
-    await connection.query(`
+    await connection.query(
+      `
     INSERT INTO clients (name, address, phone)
     VALUES ($1, $2, $3)
-    `, [name, address, phone]);
-    
+    `,
+      [name, address, phone]
+    );
+
     res.sendStatus(201);
   } catch (error) {
     console.log(error);
-    res.sendStatus(500)    
+    res.sendStatus(500);
+  }
+}
+
+export async function getOrderPerClient(req, res) {
+  const { id } = req.params;
+
+  try {
+    const result = await connection.query(
+      `
+      SELECT 
+        orders.id AS "orderId",
+        orders.quantity,
+        orders."createdAt",
+        orders."totalPrice",
+        cakes.name AS "cakeName"
+      FROM clients
+        JOIN orders ON orders."clientId"=clients.id
+        JOIN cakes ON cakes.id=orders."cakeId"
+      WHERE clients.id=$1
+    `,
+      [id]
+    );
+    if (result.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+
+    res.status(200).send(result.rows);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
   }
 }
--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { setClients } from "../controllers/clientsController.js";
+import {
+  getOrderPerClient,
+  setClients,
+} from "../controllers/clientsController.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import clientSchema from "../schemas/clientSchema.js";
 
 const clientsRouter = Router();
-clientsRouter.post('/clients', validateSchema(clientSchema), setClients)
+clientsRouter.post("/clients", validateSchema(clientSchema), setClients);
+clientsRouter.get("/clients/:id/orders", getOrderPerClient);
 
 export default clientsRouter;
